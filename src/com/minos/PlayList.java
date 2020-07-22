e3cdbf39162f10928597bf58fd1306824b780e7b
minos <tyforbyte@gmail.com>
2020-07-22 16:52:10 +0800

    Version 1.0

--- src/com/minos/PlayList.java
+++ src/com/minos/PlayList.java
@@ -12,6 +12,22 @@ public class PlayList {
         musicList = new ArrayList<>();
     }
 
+    public String getPlayListName() {
+        return playListName;
+    }
+
+    public void setPlayListName(String playListName) {
+        this.playListName = playListName;
+    }
+
+    public List<Song> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Song> musicList) {
+        this.musicList = musicList;
+    }
+
     /**
      * 将歌曲添加到播放列表
      * @param song 需添加的歌曲对象
@@ -42,6 +58,66 @@ public class PlayList {
         for (Song song:musicList){
             System.out.println(song);
         }
+    }
+
+    /**
+     * 通过歌曲ID查询
+     * @param id 歌曲ID
+     * @return 歌曲对象,返回NULL代表列表中没有该歌曲
+     */
+    public Song searchSongById(String id){
+        Song song = null;
+        for (Song song1: musicList){
+            if (song1.getId().equals(id)){
+                song = song1;
+            }
+        }
+        return song;
+    }
+
+    /**
+     * 根据歌曲名称查询
+     * @param name 歌名称曲
+     * @return
+     */
+    public Song searchSongByName(String name){
+        Song song = null;
+        for (Song song1: musicList){
+            if (song1.getName().equals(name)){
+                song = song1;
+            }
+        }
+        return song;
+    }
+
+    /**
+     * 根据ID修改歌曲信息
+     * @param id 歌曲ID
+     * @param song 歌曲名称
+     */
+    public void updateSong(String id, Song song){
+        Song song1 = searchSongById(id);
+        if (song1 == null){
+            System.out.println("没有ID："+id+"对应的歌曲信息。");
+        }else {
+            //先移除原来的信息再添加
+            musicList.remove(song1);
+            musicList.add(song);
+            System.out.println("修改成功！");
+        }
+    }
+
+    /**
+     * 删除指定歌曲信息
+     * @param id
+     */
+    public void deleteSong(String id){
+        Song song = searchSongById(id);
+        if (song != null){
+            musicList.remove(song);
+        }else {
+            System.out.println("没有ID："+id+"对应的歌曲信息。");
+        };
     }
 
 }
--- src/com/minos/Test.java
+++ src/com/minos/Test.java
@@ -1,7 +1,5 @@
 package com.minos;
 
-import java.util.Collections;
-
 public class Test {
 
     //对歌曲类Song进行测试
@@ -35,11 +33,81 @@ public class Test {
         mainPlayList.addToPlayList(song4);
 
         mainPlayList.displayAllSongs();
+
+        //通过ID查询歌曲
+        System.out.println("-----通过ID查询----- ");
+        Song song = mainPlayList.searchSongById("S002");
+        if (song != null){
+            System.out.println(song);
+        }else {
+            System.out.println("该歌曲不存在！");
+        }
+
+        //通过名称查询歌曲
+        song = null;
+        System.out.println("-----通过名称查询----- ");
+        song = mainPlayList.searchSongByName("四只猫");
+        if (song != null){
+            System.out.println(song);
+        }else {
+            System.out.println("该歌曲不存在！");
+        }
+
+        //修改歌曲信息
+        Song song5 = new Song("S005", "新歌", "新歌手");
+        mainPlayList.updateSong("S003",song5);
+        mainPlayList.displayAllSongs();
+
+        //删除歌曲信息
+        mainPlayList.deleteSong("S005");
+        mainPlayList.displayAllSongs();
+
     }
+
+    //测试PlayListCollection播放器类
+    public static void testPlayListCollection(){
+        Song song1 = new Song("S001", "两只老虎", "小太阳");
+        Song song2 = new Song("S002", "三只老鼠", "中太阳");
+        Song song3 = new Song("S003", "四只猫", "大太阳");
+
+        //创建主播放列表
+        PlayList mainPlayList = new PlayList("主播放列表");
+        mainPlayList.addToPlayList(song1);
+        mainPlayList.addToPlayList(song2);
+        mainPlayList.addToPlayList(song3);
+
+        //创建普通播放列表,从主播放列表中添加歌曲进来
+        PlayList favouritePlayList = new PlayList("最喜欢的歌曲");
+        favouritePlayList.addToPlayList(mainPlayList.getMusicList().get(0));
+        favouritePlayList.addToPlayList(mainPlayList.getMusicList().get(1));
+        favouritePlayList.displayAllSongs();
+
+        //将两个播放列表添加到播放集合中（播放器）
+        PlayListCollection plc = new PlayListCollection();
+        plc.addPlayList(mainPlayList);
+        plc.addPlayList(favouritePlayList);
+        plc.displayListName();
+
+        //根据播放列表名称查询播放列表信息，并显示所有歌曲信息
+        PlayList playList = plc.searchPlayListByName("最喜欢的歌曲");
+        playList.displayAllSongs();
+
+        //删除播放列表信息
+        System.out.println("删除前：");
+        plc.displayListName();
+        System.out.println("删除后：");
+        plc.deletePlayList(favouritePlayList);
+        plc.displayListName();
+
+
+    }
+
+
 
     public static void main(String[] args) {
 //        Test.testSong();
-        Test.testPlayList();
+//        Test.testPlayList();
+        Test.testPlayListCollection();
     }
 }
 
